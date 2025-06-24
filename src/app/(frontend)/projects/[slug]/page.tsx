1084f10a1cec3950975ddf81fee2bd237c73f79d
milan <milanzivanov@users.noreply.github.com>
2025-06-24 23:25:20 +0200

    add images

--- src/app/(frontend)/projects/[slug]/page.tsx
+++ src/app/(frontend)/projects/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { sanityFetch } from "@/sanity/lib/live";
 import { PROJECT_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
 
 export default async function Page({
   params
@@ -19,6 +21,20 @@ export default async function Page({
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+      {project?.mainImage ? (
+        <Image
+          className="flex aspect-[800/400]"
+          src={urlFor(project.mainImage)
+            .width(800)
+            .height(400)
+            .quality(80)
+            .auto("format")
+            .url()}
+          alt={project?.mainImage?.alt || ""}
+          width="800"
+          height="400"
+        />
+      ) : null}
       <h1 className="text-4xl font-bold text-balance">{project?.title}</h1>
       <hr />
       <Link href="/projects">&larr; Return to projects</Link>
