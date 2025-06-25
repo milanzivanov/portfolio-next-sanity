3fd2878caff65ee8422415d833d051fff8ceb3ce
milan <milanzivanov@users.noreply.github.com>
2025-06-25 22:15:21 +0200

    some changes to the queries

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const PROJECTS_QUERY = defineQuery(`
-  *[_type == "project" && defined(slug.current)] | order(_createdAt desc) {
+  *[_type == "project" && defined(slug.current)] | order(_createdAt asc) {
 _id, title, slug
   }
 `);
