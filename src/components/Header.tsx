507b5cc1f6e4a771da90a159e06ee9006a2ca923
milan <milanzivanov@users.noreply.github.com>
2025-07-13 12:55:05 +0200

    heading for laptop view updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
 
   return (
     <header className="fixed z-50 h-24 inset-0 bg-white/80 dark:bg-slate-900/80 flex items-center backdrop-blur-lg">
-      <div className="container py-6 px-10 md:px-0">
+      <div className="container py-6 px-10 2xl:px-0">
         <div className="flex items-center justify-between gap-5">
           <Link className="flex items-center gap-2" href="/">
             <span className="text-lg sm:text-2xl font-semibold text-black">
